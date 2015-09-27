50136c3eae9ab8a4a0eeabe7de38a8b7609ef4c6
Dany Maillard <danymaillard93b@gmail.com>
2015-09-27 19:44:07 +0200

    Set name and version of app

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -8,6 +8,11 @@ use Symfony\Component\Console\Application as BaseApplication;
 
 class Application extends BaseApplication
 {
+    public function __construct()
+    {
+        parent::__construct('CFFie', 'dev');
+    }
+
     protected function getDefaultCommands()
     {
         $commands = parent::getDefaultCommands();
