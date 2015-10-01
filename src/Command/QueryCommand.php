f4e03995c92ae00adfacdcb905901cebaaa8d7f0
Dany Maillard <danymaillard93b@gmail.com>
2015-10-01 19:12:56 +0200

    #1 Add choices of stops

--- src/Command/QueryCommand.php
+++ src/Command/QueryCommand.php
@@ -11,29 +11,57 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 
 class QueryCommand extends Command
 {
+    /** @var CffClient */
+    private $cff;
+
+    /** @var SymfonyStyle */
+    private $output;
+
     protected function configure()
     {
         $this
             ->setName('query')
             ->setDescription('Query CFF connections')
-            ->addArgument('departure', InputArgument::REQUIRED, 'departure city')
-            ->addArgument('arrival', InputArgument::REQUIRED, 'arrival city')
+            ->addArgument('departure', InputArgument::REQUIRED, 'departure stop')
+            ->addArgument('arrival', InputArgument::REQUIRED, 'arrival stop')
             ->addArgument('datetime', InputArgument::OPTIONAL, 'datetime of travel', 'now')
         ;
     }
 
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        parent::initialize($input, $output);
+        $this->output = new SymfonyStyle($input, $output);
+        $debug = $output->getVerbosity() === OutputInterface::VERBOSITY_DEBUG;
+        $this->cff = new CffClient($debug);
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output = new SymfonyStyle($input, $output);
-        $debug = $output->getVerbosity() === OutputInterface::VERBOSITY_DEBUG;
-        $cff = new CffClient($debug);
-
         $date = new \DateTime($input->getArgument('datetime'));
-        $departure = $cff->getStop($input->getArgument('departure'));
-        $arrival = $cff->getStop($input->getArgument('arrival'));
-        $output->title(sprintf('%s -> %s (%s)', $departure['value'], $arrival['value'], $date->format('d:m:Y H:i:s')));
 
-        $times = $cff->query($departure, $arrival, $date);
-        $output->table(array('Departure', 'Arrival', 'Duration', 'Chg.', 'Travel with'), $times);
+        // Get departure/arrival stops
+        $departure = $this->getStop($input->getArgument('departure'), 'Which departure ?');
+        $arrival = $this->getStop($input->getArgument('arrival'), 'Which arrival ?');
+        $this->output->title(sprintf('%s -> %s (%s)', $departure['value'], $arrival['value'], $date->format('d:m:Y H:i:s')));
+
+        // Query
+        $times = $this->cff->query($departure, $arrival, $date);
+        $this->output->table(array('Departure', 'Arrival', 'Duration', 'Chg.', 'Travel with'), $times);
+    }
+
+    private function getStop($value, $question = 'Which station ?')
+    {
+        $stops = $this->cff->getStop($value, false);
+
+        if ($stops[0]['value'] != $value) {
+            $values = array_column($stops, 'value');
+            $value = $this->output->choice($question, $values, $stops[0]['value']);
+            $key = array_search($value, $values);
+
+            return $stops[$key];
+        }
+
+        return $stops[0];
     }
 }
