4a473ccfc6b686bfd92881e939c015b6dc758119
Dany Maillard <danymaillard93b@gmail.com>
2016-06-18 21:15:08 +0200

    Add notify option in query command

--- src/Command/QueryCommand.php
+++ src/Command/QueryCommand.php
@@ -3,9 +3,12 @@
 namespace Cffie\Command;
 
 use Cffie\Cff\CffClient;
+use Joli\JoliNotif\Notification;
+use Joli\JoliNotif\NotifierFactory;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -15,43 +18,71 @@ class QueryCommand extends Command
     private $cff;
 
     /** @var SymfonyStyle */
-    private $output;
+    private $io;
 
     protected function configure()
     {
         $this
             ->setName('query')
-            ->setDescription('Query CFF connections')
-            ->addArgument('departure', InputArgument::REQUIRED, 'departure stop')
-            ->addArgument('arrival', InputArgument::REQUIRED, 'arrival stop')
-            ->addArgument('datetime', InputArgument::OPTIONAL, 'datetime of travel', 'now')
+            ->setDescription('Query SBB/CFF/FFS connections')
+            ->addArgument('departure', InputArgument::REQUIRED, 'Departure stop')
+            ->addArgument('arrival', InputArgument::REQUIRED, 'Arrival stop')
+            ->addArgument('datetime', InputArgument::OPTIONAL, 'Datetime of travel', 'now')
+            ->addOption('notify', null, InputOption::VALUE_NONE, 'Show desktop notification')
         ;
     }
 
     protected function initialize(InputInterface $input, OutputInterface $output)
     {
         parent::initialize($input, $output);
-        $this->output = new SymfonyStyle($input, $output);
+        $this->io = new SymfonyStyle($input, $output);
         $debug = $output->getVerbosity() === OutputInterface::VERBOSITY_DEBUG;
         $this->cff = new CffClient($debug);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $io = new SymfonyStyle($input, $output);
         $date = new \DateTime($input->getArgument('datetime'));
 
         // Get departure/arrival stops
         $departure = $this->getStop($input->getArgument('departure'), 'Which departure ?');
         $arrival = $this->getStop($input->getArgument('arrival'), 'Which arrival ?');
-        $this->output->title(sprintf('%s -> %s (%s)', $departure['value'], $arrival['value'], $date->format('d:m:Y H:i:s')));
+        $io->title(sprintf('%s -> %s (%s)', $departure['value'], $arrival['value'], $date->format('d.m.y H:i')));
 
         // Query
         $times = $this->cff->query($departure, $arrival, $date);
-        $times = array_map(function($v) {
+
+        // Compute delays
+        $delays = array();
+        $now = new \DateTime();
+        foreach ($times as $time) {
+            $diff = $now->diff(new \DateTime($time['departure']));
+            $delays[] = $diff->h * 60 + $diff->i;
+        }
+
+        // Show output table
+        $formattedTimes = array_map(function($v, $d) {
+            $v = array('in' => $d.'´') + $v;
             $v['infos'] = '<error>'.$v['infos'].'</error>';
             return $v;
-        }, $times);
-        $this->output->table(array('Departure', 'Arrival', 'Duration', 'Chg.', 'Travel with', 'Infos'), $times);
+        }, $times, $delays);
+        $io->table(array('In', 'Dep.', 'Arr.', 'Dur.', 'Chg.', 'With', 'Infos'), $formattedTimes);
+
+        // Show notification
+        if ($input->getOption('notify')) {
+            $notifier = NotifierFactory::create();
+            $body = '';
+            foreach ($times as $t => $time) {
+                $body .= sprintf("%s - %s | %s | %s chg. | %s | in %s´ %s\r\n", $time['departure'], $time['arrival'], $time['duration'], $time['change'], $time['product'], $delays[$t], $time['infos']);
+            }
+            $notification = (new Notification())
+                ->setTitle(sprintf("%s -> %s", $departure['value'], $arrival['value']))
+                ->setBody($body)
+                ->setIcon(__DIR__.'/../../cff.jpg')
+            ;
+            $notifier->send($notification);
+        }
     }
 
     private function getStop($value, $question = 'Which station ?')
