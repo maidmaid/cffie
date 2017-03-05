c5285962c215a20b2c4520a73db90311b1bba09c
Dany Maillard <danymaillard93b@gmail.com>
2017-03-05 13:47:34 +0100

    Update tests

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -3,9 +3,25 @@
 namespace Cffie\Cff\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Filesystem\Filesystem;
 
 class IntegrationTest extends TestCase
 {
+    /** @var string The root directory */
+    private $rootDir;
+    /** @var Filesystem The Filesystem component */
+    private $fs;
+
+    public function setUp()
+    {
+        $this->rootDir = realpath(__DIR__.'/..');
+        $this->fs = new Filesystem();
+
+        if (!$this->fs->exists($this->rootDir.'/cffie.phar')) {
+            throw new \RuntimeException(sprintf("'cffie.phar' file in the '%s' directory does not exist.", $this->rootDir));
+        }
+    }
+
     public function testA()
     {
         $this->assertTrue(true);
