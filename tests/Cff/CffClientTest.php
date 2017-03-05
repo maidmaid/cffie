4b77522ef223633bc3745772c9e2ec66ef80568f
Dany Maillard <danymaillard93b@gmail.com>
2017-03-05 13:18:16 +0100

    Add phpunit files

---
+++ tests/Cff/CffClientTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cffie\Cff\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class CffClientTest extends TestCase
+{
+    public function testA()
+    {
+        $this->assertTrue(true);
+    }
+}
