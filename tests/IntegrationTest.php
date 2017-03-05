8ad837d19554e215d58bb92a614c24605bdab0e2
Dany Maillard <danymaillard93b@gmail.com>
2017-03-05 15:03:42 +0100

    Improve integration tests

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -4,6 +4,8 @@ namespace Cffie\Cff\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
 class IntegrationTest extends TestCase
 {
@@ -22,8 +24,56 @@ class IntegrationTest extends TestCase
         }
     }
 
-    public function testA()
+    /**
+     * Runs the given string as a command and returns the resulting output.
+     *
+     * @param string $command The name of the command to execute
+     *
+     * @return string The output of the command
+     *
+     * @throws ProcessFailedException If the command execution is not successful
+     */
+    private function runCommand($command)
     {
-        $this->assertTrue(true);
+        $process = new Process($command);
+        $process->setWorkingDirectory($this->rootDir);
+        $process->mustRun();
+
+        return $process->getOutput();
+    }
+
+    public function testDefaultCommand()
+    {
+        $output = $this->runCommand(sprintf('php cffie.phar'));
+        $this->assertRegExp('/CFFie \d\.\d\.\d by Dany Maillard/', $output);
+    }
+
+    public function testVersion()
+    {
+        $output = $this->runCommand(sprintf('php cffie.phar --version'));
+        $this->assertRegExp('/CFFie \d\.\d\.\d by Dany Maillard/', $output);
+    }
+
+    /**
+     * @dataProvider getQueryProvider
+     */
+    public function testQueryCommand($args)
+    {
+        $output = $this->runCommand(sprintf('php cffie.phar query '.$args));
+        $this->assertRegExp('/.* -> .* (.*)/', $output, 'title output');
+        $this->assertRegExp('/In.*Dep..*Arr..*Dur..*Chg..*With.*Infos/', $output, 'header output');
+        $this->assertRegExp('/\d´.*\d:\d.*\d:\d.*\d:\d.*\d/', $output, 'body output');
+    }
+
+    public function getQueryProvider()
+    {
+        return [
+            ['Lausanne Zurich'],
+            ['Lausanne Zurich 23:59'],
+            ['Lausanne Zurich --notify'],
+            ['"Sion, gare" "Berne Airport"'],
+            ['"Sion, gare" "Berne Airport" "tomorrow 12:00"'],
+            ['Lausann Zuric'],
+        ];
     }
 }
